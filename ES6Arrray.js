4e119660a7990b39a4648bbe1c30ee4f7d334d83
Shahed Sultan <sultancox34@gmail.com>
2022-12-03 20:54:08 +0600

    Add files via upload

---
+++ ES6Arrray.js
@@ -0,0 +1,11 @@
+//  just using break or continue
+var numbers=[0,2,5,3,6,9,8,7,8,5,8];
+
+for(let number of numbers){
+    // complex condition ar use kora lage nay
+    if(number===6){
+        break;
+    }
+    console.log(number);
+}
+// 
